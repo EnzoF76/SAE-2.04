636065b507b58fc75f91657557268b16fa29fa30
EnzoF76 <81768542+EnzoF76@users.noreply.github.com>
2022-06-01 15:03:37 +0200

    Update r2.sql

--- phase1/r2.sql
+++ phase1/r2.sql
@@ -1,1 +1,4 @@
-
+SELECT    cl.numClient, nomClient, quantite AS "nbA"
+FROM      Client cl JOIN Vente v     ON cl.numClient = v.numClient
+			              JOIN Concerner c ON v.numVente = c.numVente
+ORDER BY  quantite DESC;
