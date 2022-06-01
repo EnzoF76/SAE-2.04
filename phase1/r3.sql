fc023a9a0e22f93132200118be8ec58188aac9f8
EnzoF76 <81768542+EnzoF76@users.noreply.github.com>
2022-06-01 15:27:17 +0200

    Update r3.sql

--- phase1/r3.sql
+++ phase1/r3.sql
@@ -1,1 +1,6 @@
-
+SELECT		cl.numClient, nomClient, SUM(prixVente*quantite) AS "coutA"
+FROM		Client cl JOIN Vente v ON cl.numClient = v.numClient
+		  	  JOIN Concerner c ON v.numVente = c.numVente
+GROUP BY	cl.numClient
+HAVING 		SUM(prixVente*quantite) >= 50000
+ORDER BY	SUM(prixVente*quantite) DESC;
